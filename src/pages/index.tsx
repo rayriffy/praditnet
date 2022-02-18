209f044ce4354c46c1b4701fdcdd24ab23254bfd
Phumrapee Limpianchop <git@rayriffy.com>
2022-02-19 07:12:23 +0900

    chore: staged changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,24 @@ import { NextPage } from 'next'
 
 const Page: NextPage = () => {
   return (
-    <h1 className="text-2xl font-bold">OK</h1>
+    <div>
+      <div className="mx-auto max-w-2xl mt-12">
+        <div className="grid grid-cols-2 gap-8">
+          <div className="bg-gradient-to-r from-sky-500 to-blue-400 px-5 py-4 rounded-2xl relative text-white pt-14">
+            <div className="h-24 absolute -top-10 left-0 right-0">
+              <img src="/assets/logo/maimai.png" className="h-full w-auto mx-auto" />
+            </div>
+            ok
+          </div>
+          <div className="bg-gradient-to-r from-amber-500 to-yellow-400 px-5 py-4 rounded-2xl relative text-white pt-14">
+            <div className="h-24 absolute -top-10 left-0 right-0">
+              <img src="/assets/logo/chunithm.png" className="h-full w-auto mx-auto" />
+            </div>
+            ok
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
