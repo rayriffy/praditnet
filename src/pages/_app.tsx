2185257988fd4718af6a525792b763a8ca465165
Phumrapee Limpianchop <git@rayriffy.com>
2022-02-19 08:55:13 +0900

    chore: staged changes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,25 @@
+import { Fragment } from 'react'
+
 import { NextPage } from 'next'
 import { AppProps } from 'next/app'
+import Head from 'next/head'
 
 import '../styles/tailwind.css'
 
 const NextApp: NextPage<AppProps> = props => {
   const { Component, pageProps } = props
 
-  return <Component {...pageProps} />
+  return (
+    <Fragment>
+      <Head>
+        <title>PraditNET</title>
+      </Head>
+      <Component {...pageProps} />
+      <footer className="flex justify-center mt-10">
+        <p className="text-gray-500">© 2022 Pradit Amusement</p>
+      </footer>
+    </Fragment>
+  )
 }
 
 export default NextApp
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,8 +6,9 @@ const Page: NextPage = () => {
   return (
     <div>
       <div className="mx-auto max-w-3xl mt-12 px-6">
-        <div className="flex justify-end">
-          <div className="border rounded px-2 py-1 flex items-center">
+        <div className="flex justify-between items-center mb-8">
+          <img className="h-8 w-auto" src="/assets/pamuse.svg" />
+          <div className="border rounded px-2 py-1 flex items-center hover:cursor-pointer">
             <p className="text-gray-700 font-medium mr-2">
               5000 0000 0000 0000
             </p>
@@ -16,12 +17,10 @@ const Page: NextPage = () => {
         </div>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-x-8 gap-y-16 mt-14">
           <div className="bg-gradient-to-r from-fuchsia-500 to-purple-500 hover:shadow-lg hover:shadow-purple-500 px-5 py-4 rounded-2xl relative text-white pt-14 transition duration-300 hover:scale-105 hover:cursor-pointer">
-            <div className="h-24 absolute -top-10 left-0 right-0">
-              <img
-                src="/assets/logo/finale.png"
-                className="h-full w-auto mx-auto"
-              />
-            </div>
+            <img
+              src="/assets/logo/finale.png"
+              className="h-24 w-auto mx-auto absolute -top-10 left-0 right-0"
+            />
             <div className="mt-1 py-1 px-4 flex justify-between items-center">
               <div>
                 <p className="text-lg font-semibold">Ray</p>
@@ -31,12 +30,10 @@ const Page: NextPage = () => {
             </div>
           </div>
           <div className="bg-gradient-to-r from-amber-500 to-yellow-400 hover:shadow-lg hover:shadow-yellow-500 px-5 py-4 rounded-2xl relative text-white pt-14 transition duration-300 hover:scale-105 hover:cursor-pointer">
-            <div className="h-24 absolute -top-10 left-0 right-0">
-              <img
-                src="/assets/logo/chunithm.png"
-                className="h-full w-auto mx-auto"
-              />
-            </div>
+            <img
+              src="/assets/logo/chunithm.png"
+              className="h-24 w-auto mx-auto absolute -top-10 left-0 right-0"
+            />
             <div className="mt-1 py-1 px-4 flex justify-between items-center">
               <div>
                 <p className="text-lg font-semibold">Ray</p>
@@ -46,12 +43,10 @@ const Page: NextPage = () => {
             </div>
           </div>
           <div className="bg-gradient-to-r from-neutral-500 to-zinc-500 hover:shadow-lg hover:shadow-zinc-500 px-5 py-4 rounded-2xl relative text-white pt-14 transition duration-300 hover:scale-105 hover:cursor-pointer">
-            <div className="h-24 absolute -top-10 left-0 right-0">
-              <img
-                src="/assets/logo/ongeki.png"
-                className="h-full w-auto mx-auto"
-              />
-            </div>
+            <img
+              src="/assets/logo/ongeki.png"
+              className="h-24 w-auto mx-auto absolute -top-10 left-0 right-0"
+            />
             <div className="mt-1 py-1 px-4 flex justify-between items-center">
               <div>
                 <p className="text-lg font-semibold">RAY</p>
@@ -61,12 +56,10 @@ const Page: NextPage = () => {
             </div>
           </div>
           <div className="bg-gradient-to-r from-sky-500 to-blue-400 hover:shadow-lg hover:shadow-blue-500 px-5 py-4 rounded-2xl relative text-white pt-14 transition duration-300 hover:scale-105 hover:cursor-not-allowed grayscale">
-            <div className="h-24 absolute -top-10 left-0 right-0">
-              <img
-                src="/assets/logo/maimai.png"
-                className="h-full w-auto mx-auto"
-              />
-            </div>
+            <img
+              src="/assets/logo/maimai.png"
+              className="h-24 w-auto mx-auto absolute -top-10 left-0 right-0"
+            />
             <div className="mt-1 py-1 px-4 flex justify-center items-center h-full">
               <p className="text-sm">Unavailable</p>
             </div>
