baa3500e2e01d93681e0c17df9e0e01273afad9f
Phumrapee Limpianchop <git@rayriffy.com>
2022-02-19 17:05:04 +0900

    chore: staged chagnes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,8 @@ import { NextPage } from 'next'
 import { AppProps } from 'next/app'
 import Head from 'next/head'
 
+import { CreditCardIcon } from '@heroicons/react/solid'
+
 import '../styles/tailwind.css'
 
 const NextApp: NextPage<AppProps> = props => {
@@ -14,7 +16,18 @@ const NextApp: NextPage<AppProps> = props => {
       <Head>
         <title>PraditNET</title>
       </Head>
-      <Component {...pageProps} />
+      <div className="mx-auto max-w-3xl mt-12 px-6">
+        <header className="flex justify-between items-center mb-8">
+          <img className="h-8 w-auto" src="/assets/pamuse.svg" />
+          <div className="border rounded px-2 py-1 flex items-center hover:cursor-pointer">
+            <p className="text-gray-700 font-medium mr-2">8594</p>
+            <CreditCardIcon className="w-6 h-6 text-gray-700" />
+          </div>
+        </header>
+        <main>
+          <Component {...pageProps} />
+        </main>
+      </div>
       <footer className="flex justify-center mt-10">
         <p className="text-gray-500">© 2022 Pradit Amusement</p>
       </footer>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,13 @@
+import { Fragment } from 'react'
+
 import { GetServerSideProps, NextPage } from 'next'
 
 import { ArrowRightIcon, CreditCardIcon } from '@heroicons/react/solid'
 
-import { UserPreview } from '../modules/home/@types/UserPreview'
 import { games } from '../modules/home/constants/games'
 import { PreviewCard } from '../modules/home/components/previewCard'
+
+import { UserPreview } from '../modules/home/@types/UserPreview'
 
 interface Props {
   finale: UserPreview
@@ -14,25 +17,14 @@ interface Props {
 
 const Page: NextPage<Props> = props => {
   return (
-    <div>
-      <div className="mx-auto max-w-3xl mt-12 px-6">
-        <div className="flex justify-between items-center mb-8">
-          <img className="h-8 w-auto" src="/assets/pamuse.svg" />
-          <div className="border rounded px-2 py-1 flex items-center hover:cursor-pointer">
-            <p className="text-gray-700 font-medium mr-2">8594</p>
-            <CreditCardIcon className="w-6 h-6 text-gray-700" />
-          </div>
-        </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-x-8 gap-y-16 mt-14">
-          {games.map(game => (
-            <PreviewCard
-              key={`preview-${game.id}`}
-              game={game}
-              userPreview={props[game.id]}
-            />
-          ))}
-        </div>
-      </div>
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-x-8 gap-y-16 mt-14">
+      {games.map(game => (
+        <PreviewCard
+          key={`preview-${game.id}`}
+          game={game}
+          userPreview={props[game.id]}
+        />
+      ))}
     </div>
   )
 }
@@ -52,10 +44,7 @@ export const getServerSideProps: GetServerSideProps<Props> = async ctx => {
   const chunithmUserPreview = await getChunithmUserPreview()
   const ongekiUserPreview = await getOngekiUserPreview()
 
-  ctx.res.setHeader(
-    'Cache-Control',
-    'max-age=300, public'
-  )
+  ctx.res.setHeader('Cache-Control', 'max-age=300, public')
 
   return {
     props: {
