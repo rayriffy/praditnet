48e3c88d7dce2450d7a73e2bfcc89ec5058cdd9d
Phumrapee Limpianchop <git@rayriffy.com>
2022-02-21 06:25:28 +0900

    chore: staged changes

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -3,7 +3,20 @@ import NextAuth from 'next-auth'
 import FacebookProvider from "next-auth/providers/facebook"
 import AppleProvider from "next-auth/providers/apple"
 
+import { Sequelize } from "sequelize"
+import SequelizeAdapter from "@next-auth/sequelize-adapter"
+
+const {
+  DATABASE_USER,
+  DATABASE_PASS,
+  DATABASE_HOST,
+  DATABASE_PORT,
+} = process.env
+
+const sequelize = new Sequelize(`mariadb://${DATABASE_USER}:${DATABASE_PASS}@${DATABASE_HOST}:${DATABASE_PORT}/praditnet`)
+
 export default NextAuth({
+  adapter: SequelizeAdapter(sequelize),
   providers: [
     FacebookProvider({
       clientId: process.env.FACEBOOK_CLIENT_ID,
