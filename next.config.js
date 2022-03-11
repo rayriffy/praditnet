c5d47d5fe38c517d070eb24528fe8c33c95ac9d4
Phumrapee Limpianchop <git@rayriffy.com>
2022-03-12 05:32:08 +0900

    feat: pwa

--- next.config.js
+++ next.config.js
@@ -1,26 +1,48 @@
+const withPlugins = require('next-compose-plugins')
+
+const withPWA = require('next-pwa')
 const withPreact = require('next-plugin-preact')
 
-module.exports = withPreact({
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  env: {
-    RECAPCHA_SITE_KEY: process.env.RECAPCHA_SITE_KEY,
-  },
-  images: {
-    loader: 'custom',
-  },
-  async headers() {
-    return [
+const { runtimeCaching } = require('./runtimeCaching')
+
+module.exports = withPlugins(
+  [
+    [withPreact],
+    [
+      withPWA,
       {
-        source: '/assets/:path*',
-        headers: [
-          {
-            key: 'cache-control',
-            value: 'public, max-age=2592000',
-          },
-        ],
+        pwa: {
+          disable: process.env.NODE_ENV === 'development',
+          dest: 'public',
+          register: true,
+          skipWaiting: true,
+          runtimeCaching,
+        },
       },
-    ]
-  },
-})
+    ],
+  ],
+  {
+    eslint: {
+      ignoreDuringBuilds: true,
+    },
+    env: {
+      RECAPCHA_SITE_KEY: process.env.RECAPCHA_SITE_KEY,
+    },
+    images: {
+      loader: 'custom',
+    },
+    async headers() {
+      return [
+        {
+          source: '/assets/:path*',
+          headers: [
+            {
+              key: 'cache-control',
+              value: 'public, max-age=2592000',
+            },
+          ],
+        },
+      ]
+    },
+  }
+)
