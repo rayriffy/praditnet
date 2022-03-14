7293edbd7aa4636d5ee2528fa30747cd654dd63b
Phumrapee Limpianchop <git@rayriffy.com>
2022-03-15 02:16:39 +0900

    chore: stageed changes

--- src/modules/chunithm/collection/components/itemList.tsx
+++ src/modules/chunithm/collection/components/itemList.tsx
@@ -1,9 +1,7 @@
-import { divide } from 'lodash'
-import { memo, MutableRefObject, useState } from 'react'
+import { memo, MutableRefObject, useEffect, useState } from 'react'
 
 import ReCAPTCHA from 'react-google-recaptcha'
 import InfiniteScroll from 'react-infinite-scroll-component'
-import { Spinner } from '../../../../core/components/spinner'
 
 import { CollectionType } from '../constants/collectionTypes'
 import { Item } from './item'
@@ -29,6 +27,15 @@ export const ItemList = memo<Props>(props => {
   const [hasMore, setHasMore] = useState(true)
   const [current, setCurrent] = useState(items.slice(count.prev, count.next))
 
+  useEffect(() => {
+    setCount({
+      prev: 0,
+      next: 100,
+    })
+    setHasMore(true)
+    setCurrent(items.slice(0, 100))
+  }, [items])
+
   const getMoreData = () => {
     if (current.length === items.length) {
       setHasMore(false)
@@ -38,32 +45,31 @@ export const ItemList = memo<Props>(props => {
       setCurrent(
         current.concat(items.slice(count.prev + 100, count.next + 100))
       )
-    }, 1000)
+    }, 2000)
     setCount(prev => ({ prev: prev.prev + 100, next: prev.next + 100 }))
   }
 
   return (
     <InfiniteScroll
-      dataLength={current.length}
+      dataLength={items.length}
       next={getMoreData}
       hasMore={hasMore}
+      className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-6 mx-auto"
       loader={
         <div className="mt-6 flex justify-center">
           <h1 className="text-base text-gray-700 font-semibold">Loading...</h1>
         </div>
       }
     >
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-6 mx-auto">
-        {current &&
-          current.map(item => (
-            <Item
-              collection={type}
-              item={item}
-              key={`collection-item-${type.id}-${item.id}`}
-              capchaRef={capchaRef}
-            />
-          ))}
-      </div>
+      {current &&
+        current.map(item => (
+          <Item
+            collection={type}
+            item={item}
+            key={`collection-item-${type.id}-${item.id}`}
+            capchaRef={capchaRef}
+          />
+        ))}
     </InfiniteScroll>
   )
 })
