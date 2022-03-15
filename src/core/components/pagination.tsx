846982aed7931e3c71f7ac2d1217f45a1906043f
Phumrapee Limpianchop <git@rayriffy.com>
2022-03-16 04:33:58 +0900

    chore: staged changes

--- src/core/components/pagination.tsx
+++ src/core/components/pagination.tsx
@@ -8,6 +8,7 @@ import {
   ChevronDoubleLeftIcon,
   ChevronDoubleRightIcon,
 } from '@heroicons/react/solid'
+import { classNames } from '../services/classNames'
 
 interface Props {
   urlPrefix: string
@@ -50,39 +51,48 @@ export const Pagination = memo<Props>(props => {
   const { current, max, urlPrefix, firstLast = false } = props
 
   return (
-    <div className="flex justify-center mt-8 space-x-2 items-center">
-      {firstLast && (
-        <Button disabled={current === 1} href={urlPrefix}>
-          <ChevronDoubleLeftIcon className="mr-2 w-5 h-5" />
-          First
-        </Button>
+    <div
+      className={classNames(
+        firstLast ? 'flex-col space-y-2 sm:flex-row' : '',
+        'flex justify-center items-center mt-8 space-x-2'
       )}
-      <Button
-        disabled={current === 1}
-        href={`${urlPrefix}${current - 1 !== 1 ? `/${current - 1}` : ''}`}
-      >
-        <ArrowLeftIcon className="mr-2 w-5 h-5" />
-        Previous
-      </Button>
+    >
+      <div className="flex space-x-2">
+        {firstLast && (
+          <Button disabled={current === 1} href={urlPrefix}>
+            <ChevronDoubleLeftIcon className="mr-2 w-5 h-5" />
+            First
+          </Button>
+        )}
+        <Button
+          disabled={current === 1}
+          href={`${urlPrefix}${current - 1 !== 1 ? `/${current - 1}` : ''}`}
+        >
+          <ArrowLeftIcon className="mr-2 w-5 h-5" />
+          Previous
+        </Button>
+      </div>
       <p className="px-2 text-gray-700">
         Page{' '}
         <span className="font-bold">
           {current}/{max}
         </span>
       </p>
-      <Button disabled={current === max} href={`${urlPrefix}/${current + 1}`}>
-        Next
-        <ArrowRightIcon className="ml-2 w-5 h-5" />
-      </Button>
-      {firstLast && (
-        <Button
-          disabled={current === max}
-          href={`${urlPrefix}${max === 1 ? '' : `/${max}`}`}
-        >
-          Last
-          <ChevronDoubleRightIcon className="ml-2 w-5 h-5" />
+      <div className="flex space-x-2">
+        <Button disabled={current === max} href={`${urlPrefix}/${current + 1}`}>
+          Next
+          <ArrowRightIcon className="ml-2 w-5 h-5" />
         </Button>
-      )}
+        {firstLast && (
+          <Button
+            disabled={current === max}
+            href={`${urlPrefix}${max === 1 ? '' : `/${max}`}`}
+          >
+            Last
+            <ChevronDoubleRightIcon className="ml-2 w-5 h-5" />
+          </Button>
+        )}
+      </div>
     </div>
   )
 })
