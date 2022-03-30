b52c2135e3222ece40787fa60b3df98f431a6070
Phumrapee Limpianchop <git@rayriffy.com>
2022-03-30 16:09:12 +0900

    chore: staged changes

--- src/pages/event/[eventId]/index.tsx
+++ src/pages/event/[eventId]/index.tsx
@@ -51,18 +51,25 @@ const Page: NextPage<Props> = props => {
         <Preview eventId={event.id} musics={musics} />
       ) : (
         <div>
-          <div className="bg-gray-100 rounded-md px-5 py-4 block sm:flex sm:justify-between">
+          <div className="bg-gray-50 rounded-md px-5 py-4 block sm:flex sm:justify-between">
             <div className="flex justify-between sm:justify-start space-x-0 sm:space-x-4">
-              <img src={`/assets/logo/${entry.game}.png`} className="w-40" />
+              <img
+                src={`/assets/logo/${entry.game}.png`}
+                className="w-40 h-auto"
+              />
               <div className="flex items-center">
                 <p className="py-1 px-6 font-semibold text-lg bg-white rounded-md">
                   {entry.inGameName}
                 </p>
               </div>
             </div>
-            <div>
+            <div className="flex items-center mt-2 justify-end sm:block">
               <p>Remaining attempts</p>
-              <p>{entry.remainingAttempts}</p>
+              <div className="ml-2 sm:ml-0 flex sm:mt-2 justify-start sm:justify-center">
+                <p className="bg-white font-semibold text-lg rounded px-4 py-1">
+                  {entry.remainingAttempts}
+                </p>
+              </div>
             </div>
           </div>
           <h1 className="text-2xl font-bold text-gray-900 mt-4 mb-3">
@@ -84,7 +91,8 @@ const Page: NextPage<Props> = props => {
                 <div className="ml-4">
                   <h2 className="text-xl font-bold">{music.name}</h2>
                   <p className="mt-3 text-3xl font-light">
-                    {entry.attemptLog.find(o => o[0] === music.id)?.[1] ?? 0}%
+                    {entry.attemptLog.find(o => o[0] === music.id)?.[1] ?? 0}
+                    {entry.game === 'maimai' && '%'}
                   </p>
                 </div>
               </div>
