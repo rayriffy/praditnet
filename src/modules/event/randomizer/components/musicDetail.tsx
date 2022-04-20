8cce33cce5379fe4340a2a5114d76c2ef850e2b3
Phumrapee Limpianchop <git@rayriffy.com>
2022-04-20 21:12:32 +0900

    refactor: staged changes

--- src/modules/event/randomizer/components/musicDetail.tsx
+++ src/modules/event/randomizer/components/musicDetail.tsx
@@ -1,6 +1,5 @@
 import { Fragment, memo } from 'react'
 
-import { Image } from '../../../../core/components/image'
 import { DifficultyBlock } from '../../../../core/components/difficultyBlock'
 
 interface Props {
@@ -23,10 +22,9 @@ export const MusicDetail = memo<Props>(props => {
     <div className="bg-neutral-700 shadow border px-4 py-5 rounded-lg h-[386px] md:h-[382px] xl:h-[360px] pointer-events-none">
       <div className="flex justify-center">
         <div className="flex rounded-md overflow-hidden">
-          <Image
+          <img
             src={`https://cdn.pradit.net/${event.game}/jacket/${music.id}.png`}
-            width={250}
-            height={250}
+            className="w-full h-auto"
           />
         </div>
       </div>
