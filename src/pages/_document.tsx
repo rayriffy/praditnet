e5922e81d5dfe52a4806e64fd892a050f3a593ac
Phumrapee Limpianchop <git@rayriffy.com>
2022-04-24 18:22:32 +0900

    fix: fonts

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -54,6 +54,12 @@ const Document: NextPage = () => {
         <link rel="dns-prefetch" href="https://cdn-optimizer.pradit.net" />
         <link rel="preconnect" href="https://cdn-optimizer.pradit.net" />
 
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Niramit:ital,wght@0,400;0,500;0,600;0,700;1,500&family=Prompt:wght@400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
+
         <Partytown forward={['dataLayer.push']} />
 
         <script
