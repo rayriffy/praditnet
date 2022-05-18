d07ed1b5b47fc245aa55a8c9ff9ea73526bf5403
Phumrapee Limpianchop <git@rayriffy.com>
2022-05-18 19:46:26 +0900

    fix: network protocol

--- src/pages/api/_middleware.ts
+++ src/pages/api/_middleware.ts
@@ -3,7 +3,7 @@ import { NextFetchEvent, NextRequest, NextResponse } from 'next/server'
 export function middleware(req: NextRequest, ev: NextFetchEvent) {
   if (
     process.env.NODE_ENV !== 'development' &&
-    !(req.headers.get('referer') ?? '').startsWith('http://pradit.net')
+    !(req.headers.get('referer') ?? '').startsWith('https://pradit.net')
   ) {
     return new Response('Forbidden', { status: 403 })
   } else {
