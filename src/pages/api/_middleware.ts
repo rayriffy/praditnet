98ef7c97dd1e7ff1c73e1a1c39e2e96375ae911e
Phumrapee Limpianchop <git@rayriffy.com>
2022-05-18 19:50:24 +0900

    chore: staged changes

--- src/pages/api/_middleware.ts
+++ src/pages/api/_middleware.ts
@@ -1,6 +1,7 @@
 import { NextFetchEvent, NextRequest, NextResponse } from 'next/server'
 
 export function middleware(req: NextRequest, ev: NextFetchEvent) {
+  console.log('Referer: ', req.headers.get('referer'))
   if (
     process.env.NODE_ENV !== 'development' &&
     !(req.headers.get('referer') ?? '').startsWith('https://pradit.net')
